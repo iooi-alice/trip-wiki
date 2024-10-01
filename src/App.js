3d52b813bc3f9d638b5edd846f696e1d98f90550
iooi-alice <kimhee21c@hanmail.net>
2024-10-01 15:18:16 +0900

    feat: 각 컴포넌트별 state 관리 및 페이지 URL 이동에 따른 컴포넌트 연동

---
+++ src/App.js
@@ -0,0 +1,210 @@
+import Header from './components/Header.js';
+import RegionList from './components/RegionList.js';
+import CityList from './components/CityList.js';
+import CityDetail from './components/CityDetail.js';
+import { request, requestCityDetail } from './components/api.js';
+
+export default function App($app) {
+  const getSortBy = () => {
+    if (window.location.search) {
+      return window.location.search.split('sort=')[1].split('&')[0];
+    }
+    return 'total';
+  };
+
+  const getSearchWord = () => {
+    if (window.location.search && window.location.search.includes('search=')) {
+      return window.location.search.split('search=')[1];
+    }
+    return '';
+  };
+
+  this.state = {
+    startIdx: 0,
+    sortBy: getSortBy(),
+    searchWord: getSearchWord(),
+    region: '',
+    cities: '',
+    currentPage: window.location.pathname,
+  };
+
+  const renderHeader = () => {
+    new Header({
+      $app,
+      initialState: {
+        sortBy: this.state.sortBy,
+        searchWord: this.state.searchWord,
+        currentPage: this.state.currentPage,
+      },
+      handleSortChange: async (sortBy) => {
+        const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+        history.pushState(
+          null,
+          null,
+          this.state.searchWord
+            ? pageUrl + `&search=${this.state.searchWord}`
+            : pageUrl,
+        );
+        const cities = await request(
+          0,
+          this.state.region,
+          sortBy,
+          this.state.searchWord,
+        );
+
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          sortBy: sortBy,
+          cities: cities,
+        });
+      },
+      handleSearch: async (searchWord) => {
+        history.pushState(
+          null,
+          null,
+          `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`,
+        );
+        const cities = await request(
+          0,
+          this.state.region,
+          this.state.sortBy,
+          searchWord,
+        );
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          searchWord: searchWord,
+          cities: cities,
+        });
+      },
+    });
+  };
+
+  const renderRegionList = () => {
+    new RegionList({
+      $app,
+      initialState: this.state.region,
+      handleRegion: async (region) => {
+        history.pushState(null, null, `/${region}?sort=total`);
+        const cities = await request(0, region, 'total');
+        this.setState({
+          ...this.state,
+          startIdx: 0,
+          sortBy: 'total',
+          searchWord: '',
+          region: region,
+          cities: cities,
+        });
+      },
+    });
+  };
+
+  const renderCityList = () => {
+    new CityList({
+      $app,
+      initialState: this.state.cities,
+      handleLoadMore: async () => {
+        const newStartIdx = this.state.startIdx + 40;
+        const newCities = await request(
+          newStartIdx,
+          this.state.region,
+          this.state.sortBy,
+          this.state.searchWord,
+        );
+        this.setState({
+          ...this.state,
+          startIdx: newStartIdx,
+          cities: {
+            ...this.state.cities,
+            cities: [...this.state.cities.cities, ...newCities.cities],
+            isEnd: newCities.isEnd,
+          },
+        });
+      },
+      handleItemClick: (id) => {
+        history.pushState(null, null, `/city/${id}`);
+        this.setState({
+          ...this.state,
+          currentPage: `/city/${id}`,
+        });
+      },
+    });
+  };
+
+  const renderCityDetail = async (cityId) => {
+    try {
+      const cityDetailData = await requestCityDetail(cityId);
+      new CityDetail({ $app, initialState: cityDetailData });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // 조건부 렌더링
+  const render = () => {
+    const path = this.state.currentPage;
+    $app.innerHTML = '';
+
+    if (path.startsWith('/city/')) {
+      const cityId = path.split('/city/')[1];
+      renderHeader();
+      renderCityDetail(cityId);
+    } else {
+      renderHeader();
+      renderRegionList();
+      renderCityList();
+    }
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
+    render();
+  };
+
+  window.addEventListener('popstate', async () => {
+    const urlPath = window.location.pathname;
+    const prevRegion = urlPath.replace('/', '');
+    const prevStartIdx = 0;
+    const prevSortBy = getSortBy();
+    const prevSearchWord = getSearchWord();
+    const prevCitites = await request(
+      prevStartIdx,
+      prevRegion,
+      prevSortBy,
+      prevSearchWord,
+    );
+
+    this.setState({
+      ...this.state,
+      startIdx: prevStartIdx,
+      sortBy: prevSortBy,
+      searchWord: prevSearchWord,
+      region: prevRegion,
+      cities: prevCitites,
+      currentPage: urlPath,
+    });
+  });
+
+  const init = async () => {
+    const path = this.state.currentPage;
+
+    if (path.startsWith('/city/')) {
+      render();
+    } else {
+      const cities = await request(
+        this.state.startIdx,
+        this.state.region,
+        this.state.sortBy,
+        this.state.searchWord,
+      );
+
+      this.setState({
+        ...this.state,
+        cities: cities,
+      });
+    }
+  };
+
+  init();
+}
